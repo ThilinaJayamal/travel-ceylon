947b45179b513c25129c73f83b10e581232e8244
nishmikaeka <nurannishmika22@gmail.com>
2025-09-01 16:52:03 +0530

    Make more responsive

--- frontend/src/pages/Taxi.jsx
+++ frontend/src/pages/Taxi.jsx
@@ -131,14 +131,14 @@ const Taxi = () => {
           className="h-[80vh] w-full relative md:px-14 px-7 py-12 text-white flex items-center"
         >
           <div className="text-white space-y-4">
-            <h1 className="text-5xl font-bold leading-tight">
+            <h1 className="text-3xl sm:text-5xl font-bold leading-tight">
               Plan your ride,
               <br />
               we’ll take you
               <br />
               there!
             </h1>
-            <p className="text-xl text-gray-100">
+            <p className="text-base sm:text-xl text-gray-100">
               Special offers to suit your plan.
             </p>
           </div>
@@ -151,8 +151,10 @@ const Taxi = () => {
       </section>
 
       <div className="xl:mx-12 mx-4 bg-green-200 mb-16 rounded-xl p-8 mt-36">
-        <h2 className="text-4xl mb-4 font-semibold">Drive your own</h2>
-        <p className="text-lg">
+        <h2 className="text-lg sm:text-4xl mb-2 sm:mb-4 font-semibold">
+          Drive your own
+        </h2>
+        <p className="text-sm sm:text-lg">
           Explore Sri Lanka on your terms! Whether you're a local resident or an
           international visitor with a valid driver's license, rent the perfect
           vehicle for your journey. Choose from our range of trusted
@@ -176,7 +178,7 @@ const Taxi = () => {
                 </div>
                 <button
                   onClick={() => rentVehicle(item)}
-                  className="bg-green-200 rounded-full text-black text-lg px-2 py-1 mt-2 hover:bg-green-300 cursor-pointer"
+                  className="bg-green-200 rounded-full text-black text-sm sm:text-lg px-2 py-1 mt-2 hover:bg-green-300 cursor-pointer"
                 >
                   Book Now
                 </button>
@@ -186,41 +188,47 @@ const Taxi = () => {
         </div>
       </div>
 
-      <div className="xl:mx-12 mx-4 mb-16 rounded-xl p-8 mt-12">
-        <h2 className="text-4xl font-semibold mb-4">What we offer</h2>
-        <p className="text-lg">
+      <div className="xl:mx-12 bg-white shadow-xl mx-4 sm:mb-16 rounded-xl p-8 sm:mt-12">
+        <h2 className="text-lg sm:text-4xl font-semibold mb-1 sm:mb-4">
+          What we offer
+        </h2>
+        <p className="text-xs sm:text-lg">
           Enjoy seamless travel across Sri Lanka with our safe, reliable rides.
           Choose from tuktuks, cars, or vans—all with verified drivers,
           transparent pricing, and 24/7 support. Book instantly and pay your
           way!
         </p>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-8 mt-6 w-fit mx-auto">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5 sm:gap-8 mt-6 w-fit mx-auto">
           {features.map(({ icon: Icon, title, description }, index) => (
             <div
               key={index}
-              className="size-54 bg-green-200 rounded-xl flex flex-col justify-center gap-1 items-center p-2"
+              className="size-25 sm:size-54 bg-green-200 rounded-xl flex flex-col justify-center  gap-1 items-center p-4"
             >
-              <Icon className="size-12" />
-              <h3 className="text-xl font-semibold">{title}</h3>
-              <p>{description}</p>
+              <Icon className="hidden sm:block size-12" />
+              <h3 className="text-xs text-center sm:text-xl font-semibold">
+                {title}
+              </h3>
+              <p className="text-xs sm:text-base text-black/80 text-center">
+                {description}
+              </p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="xl:mx-12 mx-4 mb-16 p-8 mt-12 grid lg:grid-cols-[2.5fr_2fr] gap-4 grid-cols-1 bg-white rounded-4xl">
-        <div className="p-4 flex flex-col justify-start items-start">
-          <h2 className="text-5xl font-semibold">
+      <div className="xl:mx-12 mx-4 mb-16 p-5 sm:p-8 mt-12 grid lg:grid-cols-[2.5fr_2fr] gap-4 grid-cols-1 bg-white rounded-4xl">
+        <div className=" flex flex-col justify-start items-start">
+          <h2 className="text-lg sm:text-5xl font-semibold">
             Where Every Journey Feels Like First Class
           </h2>
-          <p className="text-xl mt-4 text-black/90">
+          <p className="text-sm sm:text-xl mt-4 text-black/90">
             At travelCeylon, we connect you to Sri Lanka’s most reliable
             rides—from buzzing tuktuks in Colombo to luxury SUVs for scenic hill
             country routes. Every driver is verified and every fare is
             transparent, so you can explore with confidence.
           </p>
-          <p className="text-xl mt-6 text-black/70">
+          <p className="text-sm sm:text-xl mt-6 text-black/70">
             Tuktuks for quick hops . AC cars for long trips . Family-sized vans
             Instant bookings . 24/7 availability . Flight tracking for airport
             pickups Upfront fares . No surge pricing . Multiple payment options
@@ -266,25 +274,25 @@ const Taxi = () => {
         <div className="pt-4">
           <img
             src={asserts.taxibtm}
-            className="max-w-full max-h-[400px] lg:rounded-[0px] rounded-b-full mx-auto"
+            className="max-w-full max-h-[250px] sm:max-h-[400px] lg:rounded-[0px] rounded-b-full mx-auto"
             alt=""
           />
         </div>
-        <div className="p-8">
-          <h1 className="text-4xl font-semibold">
+        <div className="p-5 sm:p-8">
+          <h1 className="text-lg sm:text-4xl font-semibold">
             Partner with Sri Lanka’s Favorite Ride Platform
           </h1>
-          <p className="text-lg mt-4">
+          <p className="text-sm sm:text-lg mt-4">
             Are you a taxi, tuktuk, or private vehicle driver? Partner with
             travelCeylon and connect with thousands of travelers exploring Sri
             Lanka! From city rides to scenic tours, we match you with riders who
             value safety, fair pricing, and reliable service.
           </p>
-          <p className="text-lg font-medium mt-4">
+          <p className="text-sm sm:text-lg font-medium mt-4">
             List your property to reach thousands of travelers
           </p>
-          <button className="mt-6 px-8 py-2 rounded-full bg-green-200 text-lg cursor-pointer hover:bg-green-300">
-            List your Vehicle
+          <button className="mt-6 px-8 py-2  text-sm sm:text-lg rounded-full bg-green-200  cursor-pointer hover:bg-green-300">
+            List Your Vehicle
           </button>
         </div>
       </div>
