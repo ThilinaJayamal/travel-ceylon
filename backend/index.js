afdca47710820c15dd716750dba94648ba86e5c9
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-09-03 11:05:13 +0530

    deploy setup

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://travel-ceylon-eta.vercel.app/'
+    'https://travel-ceylon-eta.vercel.app'
 
   ],
   credentials: true,
